70131154adf900e516180294536fa5ffdd27b705
Rajeev Vokkarne <ravokkar@microsoft.com>
2016-02-01 16:37:03 -0800

    Updating client ApiVersion for GA

--- csharp/service/Microsoft.Azure.Devices/ClientApiVersionHelper.cs
+++ csharp/service/Microsoft.Azure.Devices/ClientApiVersionHelper.cs
@@ -5,6 +5,6 @@ namespace Microsoft.Azure.Devices
 {
     class ClientApiVersionHelper
     {
-        public const string ApiVersionQueryString = "api-version=2015-02-03";
+        public const string ApiVersionQueryString = "api-version=2016-02-03";
     }
 }
